025d129e84c5256a02da93ffed66fc70e73b207a
Teampir <106901407+Teampir@users.noreply.github.com>
2022-06-15 08:15:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /Pirate
+  git clone https://github.com/Teampir/d-n-1-f.git /Pirate
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Pirate
