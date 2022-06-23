f2aed8b7e2bb2db45579ddaf371aee4b00023236
Teampir <106901407+Teampir@users.noreply.github.com>
2022-06-23 14:24:36 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,8 +8,6 @@ Exᴄʟᴜꜱɪᴠᴇʟʏ Mᴀᴅᴇ Tʜɪꜱ Bᴏᴛ Fᴏʀ Tᴇᴀᴍ PIRATE..
 Did You Need This Bot Clone Then Type - /clone
 
  Sᴜᴘᴘᴏʀᴛ : @FilmPiratesGroup"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 › 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/PiratesDeveloper>S-Devil</a>
 › 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
