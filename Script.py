a00f02ad8897f1b2743bcac54ee77e6780dd0580
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-04 12:38:33 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,13 +8,6 @@ Exᴄʟᴜꜱɪᴠᴇʟʏ Mᴀᴅᴇ Tʜɪꜱ Bᴏᴛ Fᴏʀ Tᴇᴀᴍ PIRATE..
 Did You Need This Bot Clone Then Type - /clone
 
  Sᴜᴘᴘᴏʀᴛ : @FilmPiratesGroup"""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-› 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/PiratesDeveloper>S-Devil</a>
-› 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-› 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-› 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-› 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-› 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 › T-USERS: <code>{}</code>
 › T-CHATS: <code>{}</code>
@@ -30,7 +23,10 @@ ID - <code>{}</code>
 Name - {}
 """
     
-    PRIVATEBOT_TXT = """<b>PRIVATE BOT FOR YOU</b>
+    PRIVATEBOT_TXT = """
+>━━━━━━━━━᚜ Private Bot ᚛━━━━━━━━━
+<b>PRIVATE BOT FOR YOU</b>
 <b>› Do You Want A Bot Like This </b>
 <b>› With All Your Creadets</b>
-<b>› With Your Ownership</b>"""
+<b>› With Your Ownership</b>
+>━━━━━━━━━━━━━━᚜  >━━━━━━━━━━━━━━᚜"""
