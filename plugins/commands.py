0c59ed5f296e98735d88da6e17dab3d0d23ea0d5
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-04 13:00:37 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,7 +51,7 @@ async def start(client, message):
         await message.reply_chat_action("typing")
         await message.reply_sticker(
             sticker='CAACAgIAAxkBAAL2zmIgeiB-uDSaV1eOn0xLGe5iuV9iAAKaEAACxpXRS5vXvdQx7K4cIwQ',
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -93,7 +93,7 @@ async def start(client, message):
         await message.reply_chat_action("typing")
         await message.reply_sticker(
             sticker='CAACAgIAAxkBAAL2zmIgeiB-uDSaV1eOn0xLGe5iuV9iAAKaEAACxpXRS5vXvdQx7K4cIwQ',
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
