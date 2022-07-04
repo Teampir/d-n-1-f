747ea98d2bb7f078e315d7ec2a26c408b58c90fc
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-04 13:08:07 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[      
-            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ɪɴ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/KochuHeastBot?startgroup=true')
+            InlineKeyboardButton('⭕ MY JOIN GROUP ⭕', url='http://t.me/FilmPiratesGroup')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("typing")
@@ -80,7 +80,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[      
-            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ɪɴ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/KochuHeastBot?startgroup=true')
+            InlineKeyboardButton('⭕ MY JOIN GROUP ⭕', url='http://t.me/FilmPiratesGroup')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("typing")
