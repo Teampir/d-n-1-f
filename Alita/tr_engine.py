9dd14d3528e50cceea548f5ec6e0010d379c3d3f
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-18 14:12:18 +0100

    Update tr_engine.py

--- Alita/tr_engine.py
+++ Alita/tr_engine.py
@@ -1,3 +1,13 @@
+
+
+from os import getcwd
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
+
+
+env_file = f"{getcwd()}/.env"
+config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
+
 import logging
 import logging.config
 
