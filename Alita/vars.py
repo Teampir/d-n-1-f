1495ef28cf6e8248ece5347d1c4b92e3264ff052
Teampir <106901407+Teampir@users.noreply.github.com>
2022-07-18 14:14:14 +0100

    Update vars.py

--- Alita/vars.py
+++ Alita/vars.py
@@ -5,3 +5,7 @@ from prettyconf.loaders import EnvFile, Environment
 
 env_file = f"{getcwd()}/.env"
 config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
+
+DEV_USERS = [int(i) for i in config("DEV_USERS", default="1198820588").split()]
+SUDO_USERS = [int(i) for i in config("SUDO_USERS", default="1198820588").split()]
+OWNER_ID = int(config("OWNER_ID", default=1198820588))
